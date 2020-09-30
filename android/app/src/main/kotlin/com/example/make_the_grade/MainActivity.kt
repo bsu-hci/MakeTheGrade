176f7f54fae1bc988eb5235a4d7ba7bccd04f4c5
samuel turner Guernsey <34995555+wulf242@users.noreply.github.com>
2020-09-30 13:13:42 -0400

    fixed with gitignore

--- android/app/src/main/kotlin/com/example/make_the_grade/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.make_the_grade
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-/// This Widget is the main application widget.
 class MyApp extends StatelessWidget {
   static const String _title = 'Make The Grade';
 
@@ -23,28 +22,69 @@ class MyStatefulWidget extends StatefulWidget {
   _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
 }
 
-var grade = 'na';
+TextEditingController gradeOutputController = TextEditingController();
+TextEditingController possiblePointsController = TextEditingController();
+bool triage = false;
+var grade = 'n/a';
 
 class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  String calc(input) {
+  String calc() {
+    if (triage) {
+      return (triageGradingcalc(praportionfinder()));
+    } else {
+      return (conventionalGrading(percentagefinder()));
+    }
+  } // end calc()
+
+  String conventionalGrading(input) {
     if (input >= 90 && input <= 100) {
       return ('A');
-    }
+    } // end if a
     if (input >= 80 && input < 90) {
       return ('B');
-    }
+    } // end if b
     if (input >= 70 && input < 80) {
       return ('C');
-    }
+    } // end if c
     if (input >= 60 && input < 70) {
       return ('D');
-    }
+    } // end if d
     if (input < 60) {
       return ('F');
-    }
+    } // end if f
   }
 
-  TextEditingController gradeController = TextEditingController();
+  String triageGradingcalc(input) {
+    if (input >= (17 / 18) && input <= 1.0) {
+      return ('A');
+    } // end if a
+    if (input >= (5 / 6) && input < (17 / 18)) {
+      return ('B');
+    } // end if b
+    if (input >= (2 / 3) && input < (5 / 6)) {
+      return ('C');
+    } // end if c
+    if (input >= (7 / 15) && input < (2 / 3)) {
+      return ('D');
+    } // end if d
+    if (input >= 0 && input < (7 / 15)) {
+      return ('f');
+    } // end if f
+  } //end
+
+  double percentagefinder() {
+    var pointsEarned = double.parse(gradeOutputController.text);
+    var pointsPossabel = double.parse(possiblePointsController.text);
+    var percent = 100 * (pointsEarned / pointsPossabel);
+    return (percent);
+  } // end percentage finder
+
+  double praportionfinder() {
+    var pointsEarned = double.parse(gradeOutputController.text);
+    var pointsPossabel = double.parse(possiblePointsController.text);
+    var praportion = (pointsEarned / pointsPossabel);
+    return (praportion);
+  } // end percentage finder
 
   Widget build(BuildContext context) {
     return Scaffold(
@@ -56,11 +96,19 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
         children: [
           Padding(padding: EdgeInsets.all(85)),
           Text('Please input your score.'),
+          Checkbox(
+            value: triage,
+            onChanged: (bool value) {
+              setState(() {
+                triage = value;
+              });
+            },
+          ),
           SizedBox(
               width: 200,
               height: 80,
               child: TextFormField(
-                  controller: gradeController,
+                  controller: gradeOutputController,
                   decoration: const InputDecoration(
                     hintText: 'Enter your grade',
                   ),
@@ -69,13 +117,32 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
                       return 'Please enter your grade ';
                     }
                   })),
+          SizedBox(
+              width: 200,
+              height: 80,
+              child: TextFormField(
+                  controller: possiblePointsController,
+                  decoration: const InputDecoration(
+                    hintText: 'Enter the possible points',
+                  ),
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please enter the possable points ';
+                    }
+                  })),
           Text('your grade is $grade'),
+<<<<<<< Updated upstream
           FloatingActionButton.extended(
             onPressed: () =>
                 setState(() => grade = calc(int.parse(gradeController.text))),
             label: Text('Calculate Grade'),
             icon: const Icon(Icons.add),
             backgroundColor: Colors.redAccent[900],
+=======
+          RaisedButton(
+            onPressed: () => setState(() => grade = calc()),
+            child: Text("Calculate"),
+>>>>>>> Stashed changes
           )
         ],
       )),
