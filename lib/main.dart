c8e9add60a80ef31bc8f4e7a55921a760e4488d4
samuel turner Guernsey <34995555+wulf242@users.noreply.github.com>
2020-09-30 13:33:19 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -96,6 +96,7 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
         children: [
           Padding(padding: EdgeInsets.all(85)),
           Text('Please input your score.'),
+          Text('triage?'),
           Checkbox(
             value: triage,
             onChanged: (bool value) {
